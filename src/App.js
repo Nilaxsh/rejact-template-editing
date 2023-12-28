29fe451ce60d2e04b52f9c935005f586e9606740
Nilaxsh <nilaxsakana@gmail.com>
2023-12-29 00:07:17 +0530

    template

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+import Hero from './Hero';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div >
+      <Header></Header>
+  <Hero></Hero>
+  <Footer></Footer>
+  </div>
+  );
+}
+
+
+
+export default App;
